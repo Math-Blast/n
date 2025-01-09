d3e332bd01c355e1721ddf6ff55903585f2af060
MathBlast <fluxgod066@gmail.com>
2025-01-10 07:55:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function downloadGame(platform) {
     let downloadLinks = {
-        android: 'https://www.mediafire.com/file/vc37mtuclvxfyet/MathBlast.apk/file'
+        android: 'https://www.mediafire.com/file/vc37mtuclvxfyet/MathBlast.apk/file?dkey=s0og4thtqxu&r=1893'
           };
 
     if (downloadLinks[platform]) {
